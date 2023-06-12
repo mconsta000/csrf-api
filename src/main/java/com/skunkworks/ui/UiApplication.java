03f11ecd30aa3a8c6be3b9ba1ab71086d66fa001
Mike Constantino <mconstantino@truno.com>
2023-06-12 13:14:25 -0400

    add some logging

--- src/main/java/com/skunkworks/ui/UiApplication.java
+++ src/main/java/com/skunkworks/ui/UiApplication.java
@@ -1,5 +1,7 @@
 package com.skunkworks.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,22 +18,35 @@ import java.util.UUID;
 @RestController
 @RequestMapping("api")
 public class UiApplication {
+    private static final Logger log = LoggerFactory.getLogger(UiApplication.class);
 
     @GetMapping("/user")
     public Principal user(Principal user) {
+        log.debug("Returning user {}", user);
+
         return user;
     }
 
-    @GetMapping("/resource")
     public Map<String, Object> home() {
+        log.debug("home()");
+
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World");
         return model;
     }
 
+    @GetMapping("/resource")
+    public Map<String, Object> get() {
+        log.debug("GET /resource");
+
+        return home();
+    }
+
     @PostMapping("/resource")
     public Map<String, Object> post() {
+        log.debug("POST /resource");
+
         return home();
     }
 
